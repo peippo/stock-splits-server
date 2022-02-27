88d84e01ac7a0bc14fcf06b27813b0c466fe05bb
Janne Peippo <janne.peippo@poutapilvi.fi>
2022-02-27 20:24:00 +0200

    Remove pagination & count

--- services/splits.js
+++ services/splits.js
@@ -1,65 +1,36 @@
 const db = require("../services/db");
-const PER_PAGE = 20;
 const quarters = require("../utils/utils");
 
-function getByYear(params, page = 1, ticker = "") {
+function getByYear(params, ticker = "") {
 	const { year } = params;
-	const offset = (page - 1) * PER_PAGE;
 
-	let dataQuery = `SELECT * FROM splits WHERE execution_date >= '${year}-01-01' AND execution_date <= '${year}-12-31'`;
-	let countQuery = `SELECT COUNT(*) FROM splits WHERE execution_date >= '${year}-01-01' AND execution_date <= '${year}-12-31'`;
+	let query = `SELECT * FROM splits WHERE execution_date >= '${year}-01-01' AND execution_date <= '${year}-12-31'`;
 
 	if (ticker) {
-		dataQuery += ` AND ticker = '${ticker}'`;
-		countQuery += ` AND ticker = '${ticker}'`;
+		query += ` AND ticker = '${ticker}'`;
 	}
 
-	dataQuery += ` ORDER BY execution_date LIMIT ?,?`;
+	query += ` ORDER BY execution_date`;
 
-	const data = db.query(dataQuery, [offset, PER_PAGE]);
-	let count = db.query(countQuery, []);
+	const data = db.query(query, []);
 
-	count = count[0]["COUNT(*)"];
-	const meta = {
-		currentPage: parseInt(page),
-		totalPages: Math.ceil(count / PER_PAGE),
-		count,
-	};
-
-	return {
-		data,
-		meta,
-	};
+	return data;
 }
 
-function getByQuarter(params, page = 1, ticker = "") {
+function getByQuarter(params, ticker = "") {
 	const { year, quarter } = params;
-	const offset = (page - 1) * PER_PAGE;
 
-	let dataQuery = `SELECT * FROM splits WHERE execution_date >= '${year}-${quarters[quarter].start}-01' AND execution_date <= '${year}-${quarters[quarter].end}-31'`;
-	let countQuery = `SELECT COUNT(*) FROM splits WHERE execution_date >= '${year}-${quarters[quarter].start}-01' AND execution_date <= '${year}-${quarters[quarter].end}-31'`;
+	let query = `SELECT * FROM splits WHERE execution_date >= '${year}-${quarters[quarter]?.start}-01' AND execution_date <= '${year}-${quarters[quarter]?.end}-31'`;
 
 	if (ticker) {
-		dataQuery += ` AND ticker = '${ticker}'`;
-		countQuery += ` AND ticker = '${ticker}'`;
+		query += ` AND ticker = '${ticker}'`;
 	}
 
-	dataQuery += ` ORDER BY execution_date LIMIT ?,?`;
+	query += ` ORDER BY execution_date`;
 
-	const data = db.query(dataQuery, [offset, PER_PAGE]);
-	let count = db.query(countQuery, []);
+	const data = db.query(query, []);
 
-	count = count[0]["COUNT(*)"];
-	const meta = {
-		currentPage: parseInt(page),
-		totalPages: Math.ceil(count / PER_PAGE),
-		count,
-	};
-
-	return {
-		data,
-		meta,
-	};
+	return data;
 }
 
 function getByMonth(params, ticker = "") {
@@ -76,9 +47,7 @@ function getByMonth(params, ticker = "") {
 
 	const data = db.query(query, []);
 
-	return {
-		data,
-	};
+	return data;
 }
 
 module.exports = {
